c81f41e993692c48f6c295346b90ce95d8cfbe31
kirillzhakin <kirillzhakin@mail.ru>
2023-04-09 10:17:02 +0500

    get collection posts

--- index.js
+++ index.js
@@ -1,15 +1,49 @@
-require('dotenv').config();
+require("dotenv").config();
 const express = require("express");
+// const cors = require('./middlewares/cors');
+
+const {
+  initializeApp,
+  applicationDefault,
+  cert,
+} = require("firebase-admin/app");
+const {
+  getFirestore,
+  Timestamp,
+  FieldValue,
+} = require("firebase-admin/firestore");
+
 const app = express();
+// app.use(cors);
+
+const serviceAccount = require("./serviceAccountKey.json");
+
+initializeApp({
+  credential: cert(serviceAccount),
+});
+
+const db = getFirestore();
+
 const { PORT } = process.env;
 
 app.get("/posts", (_req, res) => {
-  const posts = {
-    capture: "Winter Palace",
-    location: "SP",
-  };
+  res.set("Access-Control-Allow-Origin", "*");
 
-  res.send(posts);
+  const posts = [];
+
+  db.collection("posts")
+    .orderBy("date", "desc")
+    .get()
+    .then((snapshot) => {
+      snapshot.forEach((doc) => posts.push(doc.data()));
+      res.send(posts);
+    });
+});
+
+app.get("/createPost", (_req, res) => {
+  res.set("Access-Control-Allow-Origin", "*");
+
+  res.send("createPost");
 });
 
 app.listen(PORT || 3000, () => {
